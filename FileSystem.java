703bc23beaf62bc45f6d776220ba32f1e8f7ff38
Kristijan Hornung <79140770+kristijanH1998@users.noreply.github.com>
2023-04-07 04:22:43 -0700

    Add files via upload

--- FileSystem.java
+++ FileSystem.java
@@ -2,11 +2,9 @@ package objectOrientedDesign;
 import java.util.ArrayList;
 import java.util.Date;
 
-
-
 class Directory extends RootDirectory {
-	private String name;
 	private Date dateCreated;
+	private RootDirectory parentDir;
 	public Directory(String name, long size, String owner, String permissions) {
 		super(name,size,owner,permissions);
 		Date date = new Date();
@@ -15,46 +13,139 @@ class Directory extends RootDirectory {
 	public Date getDate() {
 		return this.dateCreated;
 	}
+	public void addRootParent(RootDirectory r) {
+		this.parentDir = r;
+		this.addToPath(r.getName());
+	}
+	public void addParent(Directory d) {
+		this.parentDir = d;
+		d.childDirs.add(this);
+		this.addToPath(d.getName());
+	}
+	public void removeParent() {
+		this.parentDir = null;
+	}
+	public void addToPath(String d) {
+		if(this.parentDir instanceof RootDirectory && !(this.parentDir instanceof Directory)) {
+			return;
+		}
+		String p = this.getPath();
+		p += (d + "/");
+		this.setPath(p);
+	}
+	public void setPath(String p) {
+		this.path = p;
+	}
 }
-
 class RootDirectory{
 	private String name;
 	private long size;
-	private String path = "/root/";
-	private ArrayList<Directory> childDirs = new ArrayList<Directory>();
-	private ArrayList<File> childFiles = new ArrayList<File>();
+	public String path = "/root/";
+	protected ArrayList<Directory> childDirs = new ArrayList<Directory>();
+	protected ArrayList<File> childFiles = new ArrayList<File>();
 	private String owner;
 	private String permissions;
-
 	public RootDirectory(String name, long size, String owner, String permissions){
 		this.name = name;
 		this.size = size;
 		this.owner = owner;
 		this.permissions = permissions;
 	}
-
-	public void addChildFile(File f) {
+	public void addFile(File f) {
 		this.childFiles.add(f);
+		f.addParent(this);
+	}
+	public void removeChildDir(Directory d) {
+		this.childDirs.remove(d);
+		d.removeParent();
+	}
+	public void removeChildFile(File f) {
+		this.childFiles.remove(f);
+		f.removeParent();
 	}
 	public void addChildDirs(Directory d) {
 		this.childDirs.add(d);
+		d.addRootParent(this);
 	}
 	public String getName() {
 		return name;
 	}
+	public long getSize() {
+		return size;
+	}
+	public String getPermsAndOwner() {
+		return permissions + " " + owner;
+	}
+	public String getPath() {
+		return this.path;
+	}
+	public void showFilesAndDirs() {
+		for(File f: childFiles) {
+			System.out.println(f.getName() + " " + f.getSize() + " " + f.getDate() + " " + f.getFormat() + " " + 
+					f.getPermissions() + " " + f.getOwner() + " " + f.getPath());
+		}
+		
+		for(Directory d: childDirs) {
+			System.out.println(d.getName() + " " + d.getSize() + " " + d.getDate() + " " + 
+					d.getPermsAndOwner() + " " + d.getPath());
+		}
+	}
 }
-
 class File{
+	private RootDirectory parent;
 	private String name;
 	private long size;
-	private String path;
-	private Date dateCreated = new Date();
+	private String path = "/root/";
+	private Date dateCreated;
 	private String owner;
-	private String[] permissions = new String[4];
+	private String permissions;
 	private String format;
 	private String data;
-}
-
+	public File(String name, long size, String owner, String permissions, String format) {
+		this.name = name;
+		this.size = size;
+		this.owner = owner;
+		this.permissions = permissions;
+		this.format = format;
+		this.dateCreated = new Date();
+	}
+	public String getName(){
+		return this.name;
+	}
+	public long getSize() {
+		return this.size;
+	}
+	public Date getDate() {
+		return this.dateCreated;
+	}
+	public String getFormat() {
+		return this.format;
+	}
+	public String getPermissions() {
+		return this.permissions;
+	}
+	public String getOwner() {
+		return this.owner;
+	}
+	public String getPath() {
+		return this.path;
+	}
+	public void addParent(RootDirectory d) {
+		this.parent = d;
+		d.childFiles.add(this);
+		this.addToPath(d.getName());
+	}
+	public void removeParent() {
+		this.parent = null;
+	}
+	public void addToPath(String d) {
+		String p = this.getPath();
+		p += (d + "/");
+		this.setPath(p);
+	}
+	public void setPath(String p) {
+		this.path = p;
+}}
 public class FileSystem {
 	private String name;
 	private String fileSystemType;
@@ -75,15 +166,27 @@ public class FileSystem {
 	public String getType() {
 		return this.fileSystemType;
 	}
-
 	public static void main(String[] args) {
 		FileSystem fs = FileSystem.getInstance();
 		System.out.println(fs.getName() + "   " + fs.getType());
 		RootDirectory root = new RootDirectory("Root", 1, "Kristijan", "-rwxrwxrwx");
-		System.out.println(root.getName());
-		Directory dir1 = new Directory("Child", 1, "Kristijan", "-rwxrwxrwx");
-		System.out.println(dir1.getName());
-		System.out.println(dir1.getDate());
-
+		Directory dir1 = new Directory("Dir1", 1, "Kristijan", "-rwxrwxrwx");
+		Directory dir2 = new Directory("Dir2", 1, "Kristijan", "-rwxrwxrwx");
+		Directory dir3 = new Directory("Dir3", 1, "Kristijan", "-rwxrwxrwx");
+		root.addChildDirs(dir1);
+		root.addChildDirs(dir2);
+		root.addChildDirs(dir3);
+		Directory dir4 = new Directory("Dir4", 1, "Kristijan", "-rwxrwxrwx");
+		dir3.addChildDirs(dir4);
+		File f1 = new File("My file", 1, "Kristijan", "-rwxrwxrwx", "JPG");
+		dir2.addFile(f1);
+		root.showFilesAndDirs();
+		System.out.println();
+		dir2.showFilesAndDirs();
+		System.out.println();
+		dir3.showFilesAndDirs();
+		dir3.removeChildDir(dir4);
+		System.out.println();
+		dir3.showFilesAndDirs();
 	}
 }
