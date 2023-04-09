7aec2ea048c8f9822a2f61d2cf564ff5a6da21a3
Kristijan Hornung <79140770+kristijanH1998@users.noreply.github.com>
2023-04-09 02:39:44 -0700

    Add files via upload

--- RecursionAndDynamicProgramming.java
+++ RecursionAndDynamicProgramming.java
@@ -10,7 +10,6 @@ public class RecursionAndDynamicProgramming {
 		    sum += list.get(i);
 		return sum;
 	}
-	
 	//recursive tripleStep(problem 8.1) without memoization
 	public static void tripleStep(int start, int n, ArrayList<ArrayList<Integer>> possiblePaths, ArrayList<Integer> path){
 		if(start + 1 <= n) {
@@ -40,14 +39,61 @@ public class RecursionAndDynamicProgramming {
 			path.remove(path.size() - 1);
 		}
 	}
-	
-	public static void main(String[] args) {
-		ArrayList<ArrayList<Integer>> paths = new ArrayList<ArrayList<Integer>>();
-		ArrayList<Integer> path = new ArrayList<Integer>();
-		tripleStep(0, 5, paths, path);
-		for(ArrayList<Integer> p: paths) {
-			System.out.println(p);
+	//creates a grid for RobotInGrid problem (8.2). All cells with a value 0 are inaccessible cells, 1s are accessible cells, and -1 
+	//denotes cells that have already been visited.
+	public static int[][] makeGrid(int r, int c){
+		int[][] ar = new int[r][c];
+		for(int i = 0; i < r; i++) {
+			for(int j = 0; j < c; j++) {
+				ar[i][j] = 0;
+			}
+		}
+		ar[0][0] = 1;
+		ar[1][0] = 1;
+		ar[2][0] = 1;
+		ar[2][1] = 1;
+		ar[3][1] = 1;
+		ar[3][2] = 1;
+		ar[3][2] = 1;
+		ar[3][3] = 1;
+		for(int i = 0; i < r; i++) {
+			for(int j = 0; j < c; j++) {
+				System.out.print(ar[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		return ar;
+	}
+	//The robot starts at the upper leftmost cell in the grid (grid[0][0]), so we set that cell to -1 first to 
+	//signify that it has been visited. The goal is to reach the rightmost bottom cell (grid[3][3])
+	public static void RobotInAGrid(ArrayList<Integer> path, int[][] grid, int row, int col) {
+		path.add(grid[row][col]);
+		String output = (row == (grid.length-1) && col == (grid[0].length - 1)) ? "(" + row + ", " + col + ")"
+				: "(" + row + ", " + col + "), ";
+		System.out.print(output);
+		if(row == (grid.length-1) && col == (grid[0].length - 1)) {
+			return;
+		}
+		grid[row][col] = -1;
+		if(col + 1 < grid[0].length && grid[row][col+1] != 0 && grid[row][col+1] != -1) {
+			RobotInAGrid(path, grid, row, ++col);
+		} else if (row + 1 < grid.length && grid[row+1][col] != 0 && grid[row+1][col] != -1) {
+			RobotInAGrid(path, grid, ++row, col);
 		}
 	}
-
+	public static void main(String[] args) {
+//		ArrayList<ArrayList<Integer>> paths = new ArrayList<ArrayList<Integer>>();
+//		ArrayList<Integer> path = new ArrayList<Integer>();
+//		tripleStep(0, 5, paths, path);
+//		for(ArrayList<Integer> p: paths) {
+//			System.out.println(p);
+//		}
+		int[][] grid = makeGrid(4,4);
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		RobotInAGrid(path, grid, 0, 0);
+//		for(int i: path) {
+//			System.out.print(i + " ");
+//		}
+	}
 }
